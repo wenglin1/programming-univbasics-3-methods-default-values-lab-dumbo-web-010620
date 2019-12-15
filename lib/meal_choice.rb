054bef2b6dacd866ccfb1f194b7286777bf86086
weng lin <wenglin@buffalo.edu>
2019-12-15 03:50:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,4 @@
 def meal_choice(veg1,veg2,protein = 'meat')
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!"
   return  "A plate of #{protein} with #{veg1} and #{veg2}."
 end
